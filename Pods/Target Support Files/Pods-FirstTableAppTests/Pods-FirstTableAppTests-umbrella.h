dc362b8e9a76f0dadd7c03cc1fefdc5f100846c4
tomharman1 <tompharman@gmail.com>
2017-09-24 15:43:48 -0700

    Remove CocoaPods Pods/ dir from repo

--- Pods/Target Support Files/Pods-FirstTableAppTests/Pods-FirstTableAppTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_FirstTableAppTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_FirstTableAppTestsVersionString[];
-
